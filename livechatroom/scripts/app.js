f2c0a05c97fc29b93c45b534be445ee4858dce7d
bharathithal <98535307+bharathithal@users.noreply.github.com>
2022-02-11 00:58:32 +0530

    Add files via upload

---
+++ livechatroom/scripts/app.js
@@ -0,0 +1,64 @@
+// dom queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const rooms = document.querySelector('.chat-rooms');
+const updateMsg = document.querySelector('.update-msg');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
+
+// update the username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMsg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMsg.innerText = '', 3000);
+});
+
+// update the chat room
+rooms.addEventListener('click', e => {
+  if(e.target.tagName === 'BUTTON'){
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+});
+
+// check local storage for name
+const username = localStorage.username ? localStorage.username : 'anonymous';
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+
+// get chats & render
+chatroom.getChats(data => chatUI.render(data));
+
+const options = {
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.4s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
---
+++ livechatroom/scripts/ui.js
@@ -0,0 +1,25 @@
+// render chat templated to the DOM
+// clear the list of chats whe the room is changed
+
+class ChatUI {
+    constructor(list) {
+      this.list = list;
+    }
+    clear() {
+      this.list.innerHTML = '';
+    }
+    render(data) {
+      const when = dateFns.distanceInWordsToNow(
+        data.created_at.toDate(),
+        { addSuffix:true }
+      );
+      const html = `
+        <li class="list-group-item">
+          <span class="username">${data.username}</span>
+          <span class="message">${data.message}</span>
+          <div class="time">${when}</span>
+        </li>
+      `;
+      this.list.innerHTML += html;
+    }
+  }
